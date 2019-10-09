4667b9ccdca11cd7764cb98525011be191fca33e
danydodson <danydodson@gmail.com>
2019-10-09 04:25:27 -0500

    moved routes to seperate component

---
+++ client/src/components/routes/routes-view.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from '../home'
+import Post from '../post'
+import Editor from '../editor'
+import Login from '../login'
+import Profile from '../profile'
+import Favorites from '../favorites'
+import Register from '../register'
+import Settings from '../settings'
+
+import Private from './routes-private'
+
+const Routes = () => {
+  return (
+    <section className='container'>
+      {/* <Alert /> */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/post/:id" component={Post} />
+        <Route path="/@:username/favorites" component={Favorites} />
+        <Route path="/@:username" component={Profile} />
+        <Private path="/editor" component={Editor} />
+        <Private path="/settings" component={Settings} />
+        <Private path="/editor/:slug" component={Editor} />
+        {/* <Route component={NotFound} /> */}
+      </Switch>
+    </section>
+  )
+}
+
+export default Routes
